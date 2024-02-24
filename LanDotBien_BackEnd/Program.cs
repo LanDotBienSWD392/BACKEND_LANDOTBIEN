f0fa3e3c7ebb477cff76f356089b369525a80ba5
hoangdinh <hoangdn1309@gmail.com>
2024-02-24 21:29:29 +0700

    update Program.cs

--- LanDotBien_BackEnd/Program.cs
+++ LanDotBien_BackEnd/Program.cs
@@ -44,10 +44,10 @@ var config = new MapperConfiguration(cfg =>
     cfg.AddProfile(new AutoMapperProfile());
 });
 
-var app = builder.Build();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
