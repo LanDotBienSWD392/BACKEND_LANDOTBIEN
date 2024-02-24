798651a5b90c358251a80b21784e169ea2b234c7
datpq-dev <phamdat720749pd@gmail.com>
2024-02-24 10:48:00 +0700

    fix sql file

--- LanVarDB.sql
+++ LanVarDB.sql
@@ -1,146 +1,110 @@
 CREATE TABLE [UserPermission] (
-  [id] bigint PRIMARY KEY,
-  [Role] nvarchar(255)
-)
-GO
+  id BIGINT PRIMARY KEY,
+  Role VARCHAR(255)
+);
 
 CREATE TABLE [User] (
-  [id] bigint PRIMARY KEY,
-  [Permission_id] bigint,
-  [Package_id] bigint,
-  [IdentityCard] nvarchar(255),
-  [Name] nvarchar(255),
-  [Email] nvarchar(255),
-  [Username] nvarchar(255),
-  [Password] nvarchar(255),
-  [Image] nvarchar(255),
-  [Phone] integer,
-  [Dob] date,
-  [Address] nvarchar(255),
-  [Gender] nvarchar(255),
-  [RegisterDay] nvarchar(255),
-  [Status] bit
-)
-GO
+  id BIGINT PRIMARY KEY,
+  Permission_id BIGINT,
+  Package_id BIGINT,
+  IdentityCard VARCHAR(255),
+  Name VARCHAR(255),
+  Email VARCHAR(255),
+  Username VARCHAR(255),
+  Password VARCHAR(255),
+  Image VARCHAR(255),
+  Phone INT,
+  Dob DATE,
+  Address VARCHAR(255),
+  Gender VARCHAR(255),
+  RegisterDay VARCHAR(255),
+  Status BIT
+);
 
-CREATE TABLE [Package] (
-  [id] bigint PRIMARY KEY,
-  [PackageName] nvarchar(255),
-  [Package_Description] nvarchar(255),
-  [StartDay] date,
-  [EndDay] date,
-  [Status] bit
-)
-GO
+CREATE TABLE Package (
+  id BIGINT PRIMARY KEY,
+  PackageName VARCHAR(255),
+  Package_Description VARCHAR(255),
+  StartDay DATE,
+  EndDay DATE,
+  Status BIT
+);
 
-CREATE TABLE [Auction] (
-  [id] bigint PRIMARY KEY,
-  [Product_id] bigint,
-  [StartDay] date,
-  [AuctionDay] date,
-  [Auction_Name] nvarchar(255),
-  [Deposit_Money] double,
-  [Status] bit
-)
-GO
+CREATE TABLE Auction (
+  id BIGINT PRIMARY KEY,
+  Product_id BIGINT,
+  StartDay DATE,
+  AuctionDay DATE,
+  Auction_Name VARCHAR(255),
+  Deposit_Money FLOAT,
+  Status BIT
+);
 
-CREATE TABLE [RoomRegistrations] (
-  [id] bigint PRIMARY KEY,
-  [User_id] bigint,
-  [Auction_id] bigint,
-  [Register_time] date
-)
-GO
+CREATE TABLE RoomRegistrations (
+  id BIGINT PRIMARY KEY,
+  User_id BIGINT,
+  Auction_id BIGINT,
+  Register_time DATE
+);
 
-CREATE TABLE [Product] (
-  [id] bigint PRIMARY KEY,
-  [ISBN] nvarchar(255),
-  [User_id] bigint,
-  [Product_Name] nvarchar(255),
-  [Product_Description] nvarchar(255),
-  [Image] nvarchar(255),
-  [Product_Price] double,
-  [Type] nvarchar(255),
-  [Status] bit
-)
-GO
+CREATE TABLE Product (
+  id BIGINT PRIMARY KEY,
+  ISBN VARCHAR(255),
+  User_id BIGINT,
+  Product_Name VARCHAR(255),
+  Product_Description VARCHAR(255),
+  Image VARCHAR(255),
+  Product_Price FLOAT,
+  Type VARCHAR(255),
+  Status BIT
+);
 
-CREATE TABLE [Cart] (
-  [id] bigint PRIMARY KEY,
-  [User_id] bigint,
-  [Product_id] bigint,
-  [isSelected] bit
-)
-GO
+CREATE TABLE Cart (
+  id BIGINT PRIMARY KEY,
+  User_id BIGINT,
+  Product_id BIGINT,
+  isSelected BIT
+);
 
 CREATE TABLE [Order] (
-  [id] bigint PRIMARY KEY,
-  [User_id] bigint,
-  [Date] date,
-  [Total_Price] double
-)
-GO
+  id BIGINT PRIMARY KEY,
+  User_id BIGINT,
+  Date DATE,
+  Total_Price FLOAT
+);
 
-CREATE TABLE [OrderItem] (
-  [id] bigint PRIMARY KEY,
-  [Order_id] bigint,
-  [Product_id] bigint
-)
-GO
+CREATE TABLE OrderItem (
+  id BIGINT PRIMARY KEY,
+  Order_id BIGINT,
+  Product_id BIGINT
+);
 
-CREATE TABLE [Bill] (
-  [id] bigint PRIMARY KEY,
-  [Order_id] bigint,
-  [Payment_Method] nvarchar(255),
-  [Total_Price] double,
-  [Status] bit
-)
-GO
+CREATE TABLE Bill (
+  id BIGINT PRIMARY KEY,
+  Order_id BIGINT,
+  Payment_Method VARCHAR(255),
+  Total_Price FLOAT,
+  Status BIT
+);
 
-CREATE TABLE [Bid] (
-  [id] bigint PRIMARY KEY,
-  [Auction_id] bigint,
-  [User_id] bigint,
-  [Bid] double,
-  [Bid_time] date
-)
-GO
+CREATE TABLE Bid (
+  id BIGINT PRIMARY KEY,
+  Auction_id BIGINT,
+  User_id BIGINT,
+  Bid FLOAT,
+  Bid_time DATE
+);
 
-ALTER TABLE [Bid] ADD FOREIGN KEY ([Auction_id]) REFERENCES [Auction] ([id])
-GO
-
-ALTER TABLE [Bid] ADD FOREIGN KEY ([User_id]) REFERENCES [User] ([id])
-GO
-
-ALTER TABLE [User] ADD FOREIGN KEY ([Permission_id]) REFERENCES [UserPermission] ([id])
-GO
-
-ALTER TABLE [User] ADD FOREIGN KEY ([Package_id]) REFERENCES [Package] ([id])
-GO
-
-ALTER TABLE [User] ADD FOREIGN KEY ([id]) REFERENCES [RoomRegistrations] ([User_id])
-GO
-
-ALTER TABLE [Auction] ADD FOREIGN KEY ([id]) REFERENCES [RoomRegistrations] ([Auction_id])
-GO
-
-ALTER TABLE [Product] ADD FOREIGN KEY ([User_id]) REFERENCES [User] ([id])
-GO
-
-ALTER TABLE [User] ADD FOREIGN KEY ([id]) REFERENCES [Cart] ([User_id])
-GO
-
-ALTER TABLE [Product] ADD FOREIGN KEY ([id]) REFERENCES [Cart] ([Product_id])
-GO
-
-ALTER TABLE [OrderItem] ADD FOREIGN KEY ([Order_id]) REFERENCES [Order] ([id])
-GO
-
-ALTER TABLE [Product] ADD FOREIGN KEY ([id]) REFERENCES [Auction] ([Product_id])
-GO
-
-ALTER TABLE [Cart] ADD FOREIGN KEY ([Product_id]) REFERENCES [OrderItem] ([Product_id])
-GO
-
-ALTER TABLE [Order] ADD FOREIGN KEY ([id]) REFERENCES [Bill] ([Order_id])
-GO
+ALTER TABLE [User] ADD FOREIGN KEY (Permission_id) REFERENCES UserPermission(id);
+ALTER TABLE [User] ADD FOREIGN KEY (Package_id) REFERENCES Package(id);
+ALTER TABLE RoomRegistrations ADD FOREIGN KEY (User_id) REFERENCES [User](id);
+ALTER TABLE RoomRegistrations ADD FOREIGN KEY (Auction_id) REFERENCES Auction(id);
+ALTER TABLE Product ADD FOREIGN KEY (User_id) REFERENCES [User](id);
+ALTER TABLE Cart ADD FOREIGN KEY (User_id) REFERENCES [User](id);
+ALTER TABLE Cart ADD FOREIGN KEY (Product_id) REFERENCES Product(id);
+ALTER TABLE OrderItem ADD FOREIGN KEY (Order_id) REFERENCES [Order](id);
+ALTER TABLE OrderItem ADD FOREIGN KEY (Product_id) REFERENCES Product(id);
+ALTER TABLE Bill ADD FOREIGN KEY (Order_id) REFERENCES [Order](id);
+ALTER TABLE Bid ADD FOREIGN KEY (Auction_id) REFERENCES Auction(id);
+ALTER TABLE Bid ADD FOREIGN KEY (User_id) REFERENCES [User](id);
+ALTER TABLE Auction ADD FOREIGN KEY (Product_id) REFERENCES Product(id);
