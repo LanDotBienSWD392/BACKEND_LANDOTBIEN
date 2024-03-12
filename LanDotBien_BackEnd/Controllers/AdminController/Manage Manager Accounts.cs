785349dbf01333b6fca6be74ea3328bdb0175abb
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-03-12 16:07:16 +0700

    Thêm Method Manage Manager Account

---
+++ LanDotBien_BackEnd/Controllers/AdminController/Manage Manager Accounts.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LanDotBien_BackEnd.Controllers.AdminController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Manage_Manager_Accounts : ControllerBase
+    {
+        // GET: api/<Manage_Manager_Accounts>
+        [HttpGet]
+        public IEnumerable<string> GetAllUser()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Manage_Manager_Accounts>/5
+        [HttpGet("{id}")]
+        public string GetUserById(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Manage_Manager_Accounts>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Manage_Manager_Accounts>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Manage_Manager_Accounts>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
