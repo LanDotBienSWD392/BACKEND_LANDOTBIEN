92a9a6425e877cfa3cfd469d912b46f283181d8b
DLuong <luongpdse162038@fpt.edu.vn>
2024-03-15 16:17:08 +0700

    up code

--- LanVar.Service/Service/UserPermissionService.cs
+++ LanVar.Service/Service/UserPermissionService.cs
@@ -36,7 +36,7 @@ public class UserPermissionService : IUserPermissionService
 
     public async Task<IEnumerable<UserPermission>> GetAllRole()
     {
-        return await _userPermissionRepository.GetAllAsync(qweqw);
+        return await _userPermissionRepository.GetAllAsync();
     }
 
     public async Task<UserPermission> UpdateRole(long id, UserPermission updatedUserPermission)
