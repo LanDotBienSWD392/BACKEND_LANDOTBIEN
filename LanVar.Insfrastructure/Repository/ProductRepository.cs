5d019637de5e8d80447b901fcac968a7bef5f55d
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-03-15 16:33:02 +0700

    update code

--- LanVar.Insfrastructure/Repository/ProductRepository.cs
+++ LanVar.Insfrastructure/Repository/ProductRepository.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LanVar.DTO.AutoMapperProfile;
 
 namespace LanVar.Insfrastructure.Repository
 {
