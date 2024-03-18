e5b2781f1f71db939af8824c1eba400d26324034
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-03-18 15:23:57 +0700

    update cde

--- LanVar.DTO/DTO/response/LoginDTOResponse.cs
+++ LanVar.DTO/DTO/response/LoginDTOResponse.cs
@@ -6,4 +6,5 @@ public class LoginDTOResponse
     public required string username { get; set; }
     public required string email { get; set; }
     public required long permission_id { get; set; }
+    //
 }
