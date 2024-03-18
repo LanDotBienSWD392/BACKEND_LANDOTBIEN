bbc3aa52272c72c9a2c1c3eb8eab2b07eeabdb53
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-03-18 15:13:29 +0700

    Thêm thuộc tính vào LoginDTOResponse

--- LanVar.DTO/DTO/response/LoginDTOResponse.cs
+++ LanVar.DTO/DTO/response/LoginDTOResponse.cs
@@ -2,5 +2,8 @@ namespace LanVar.DTO.DTO.response;
 
 public class LoginDTOResponse
 {
-    public required string userName { get; set; }
+    public required long id {  get; set; }
+    public required string username { get; set; }
+    public required string email { get; set; }
+    public required long permission_id { get; set; }
 }
