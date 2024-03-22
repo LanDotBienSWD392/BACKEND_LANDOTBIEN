168bbc1633020a98b7858271c514094ce54ab10d
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-03-22 20:27:39 +0700

    update id cho getAllitemcart

--- LanVar.DTO/DTO/response/OrderItemDTOResponse.cs
+++ LanVar.DTO/DTO/response/OrderItemDTOResponse.cs
@@ -2,6 +2,7 @@ namespace LanVar.DTO.DTO.response;
 
 public class OrderItemDTOResponse
 {
+    public long id {  get; set; }
     public string product_name { get; set; }
     public string user_name { get; set; }
     public bool isSelected { get; set; }
